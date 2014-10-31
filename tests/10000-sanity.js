4e1cfdf6452bab2e404436968044f23fc3468d80
Gevik Babakhani <gevik@blendjs.com>
2014-10-31 09:28:18 +0100

    Removed old files

--- tests/10000-sanity.js
+++ tests/10000-sanity.js
@@ -1,3 +1,6 @@
-/**
- * TODO
- */
+var assert = require('assert');
+require(__dirname + "/../lib/Blend.js");
+
+assert.ok(Blend, 'Blend sanity');
+assert.ok(Blend.defineClass, 'class builder sanity');
+
