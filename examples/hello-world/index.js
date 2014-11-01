d0ea7450690938e082577420ad260f2ec603ef19
Gevik Babakhani <gevik@blendjs.com>
2014-11-01 21:10:54 +0100

    Added code doc

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -1,5 +1,11 @@
+// make BlendJS available
 require('blend-class-system');
+
+// require our main class
 require(__dirname + '/Hello/app/Main.js');
 
+// create the main object
 var app = Blend.create('Hello.app.Main');
+
+// run this baby
 app.run();
