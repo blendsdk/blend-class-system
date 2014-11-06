089816080a407aff0517e461f1647c7efd75ff2f
Gevik Babakhani <gevik@blendjs.com>
2014-11-06 14:23:59 +0100

    Updated the examples

--- examples/hello-world/index.js
+++ examples/hello-world/index.js
@@ -2,7 +2,8 @@
 require('blend-class-system');
 
 // require our main class
-require(__dirname + '/Hello/app/Main.js');
+// Not needed since 2.1.0
+// require(__dirname + '/Hello/app/Main.js');
 
 // create the main object
 var app = Blend.create('Hello.app.Main');
